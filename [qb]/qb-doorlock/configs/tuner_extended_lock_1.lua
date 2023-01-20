229aa5d07390bca03edb24e40c64b2552423f42f
sheshanth <sheshanth_soccer@yahoo.com>
2023-01-20 20:51:38 +0530

    initial commit

---
+++ [qb]/qb-doorlock/configs/tuner_extended_lock_1.lua
@@ -0,0 +1,16 @@
+
+
+-- -456733639 created by Demons of Pain
+Config.DoorList['tuner_extended_lock--456733639'] = {
+    locked = true,
+    hideLabel = true,
+    distance = 10,
+    objYaw = 89.999961853027,
+    fixText = false,
+    doorRate = 1.0,
+    doorType = 'garage',
+    objName = -456733639,
+    objCoords = vec3(154.809525, -3023.889160, 8.503336),
+    authorizedJobs = { ['tuner'] = 0 },
+    doorLabel = 'tuner_door_lock',
+}
