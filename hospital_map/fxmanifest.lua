918b2cffe842ddf8e87da49e5be4dc6c0aa8a3b1
sheshanth <sheshanth_soccer@yahoo.com>
2023-02-16 19:05:56 +0530

    no close combat

--- hospital_map/fxmanifest.lua
+++ hospital_map/fxmanifest.lua
@@ -2,7 +2,11 @@ fx_version 'cerulean'
 game 'gta5'
 this_is_a_map 'yes'
 
-file 'v_int_40.ytyp'
+file {
+  'v_int_40.ytyp',
+  'interiorproxies.meta'
+}
 data_file 'DLC_ITYP_REQUEST' 'stream/v_int_40.ytyp'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
 
 client_script 'ipl.lua'
